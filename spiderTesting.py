e5c7e71112dcfbd85b6a5544600b8e822081fb64
sawyerstrong <sawyerstrong28@gmail.com>
2020-01-14 00:17:36 -0500

    Updating files

--- spiderTesting.py
+++ spiderTesting.py
@@ -1,14 +1,5 @@
-#spidertesting
+#SpiderTesting
 
-
-# Bruce A. Maxwell
-# January 2015
-#
-# Pygame Tutorial Example 3
-#
-# Creates a scene with many spiders and lets the user brush them away
-# with the broom.
-#
 
 ####################### Setup #########################
 # useful imports
@@ -33,8 +24,7 @@ gameClock = pygame.time.Clock()
 
 
 # create a screen (width, height)
-screen = pygame.display.set_mode( (640, 480) )
-
+screen = pygame.display.set_mode( (800, 600) )
 ####################### Making Content #########################
 
 # load some images
@@ -49,8 +39,7 @@ afont = pygame.font.SysFont( "Helvetica", 20, bold=True )
 # render a surface with some text
 text = afont.render( "Clean up time", True, (0, 0, 0) )
 
-#create a start
-state = "start"
+
 ####################### Filling the Screen #########################
 
 # A function that draws all of the static background elements
@@ -64,14 +53,13 @@ def drawBkg(screen, text, refresh, rect=None):
 
         refresh.append( screen.get_rect() )
     else:
-        #screen.fill((255, 255, 255), rect)
+        screen.fill((255, 255, 255), rect)
 
         # blit the text surface onto the screen if it is inside the rectangle
         screen.fill( (255, 255, 255), text.get_rect().move(10, 10).clip( rect ) )
 
         trect = text.get_rect().move(10, 10) # rectangle in which to
                                              # draw the text
-                                             
         clippedRect = trect.clip( rect ) # intersection of the text
                                          # screen rectangle and the
                                          # area to update
@@ -82,10 +70,10 @@ def drawBkg(screen, text, refresh, rect=None):
 
         # refresh the rectangle
         refresh.append( rect )
-
-
+        
+#class for enemy ai
 class Enemy:  #Class for Spider character
-    def __init__(self,x, y):
+    def __init__(self, x, y):
         self.image = spider
         self.xPos = x
         self.yPos = y
@@ -95,55 +83,24 @@ class Enemy:  #Class for Spider character
         to_do = 1
     def run_away(self,player_x, player_y):
         to_do = 1
-
-
-############## Setting up the Broom as a sprite ################
-
 # get the current mouse information, and make the cursor invisible if
 # it is focused on the game window
 pygame.event.pump()
 if pygame.mouse.get_focused():
     pygame.mouse.set_visible(False)
 
-# get the mouse position and put the broom so it is centered on the
-# mouse location
-tpos = pygame.mouse.get_pos()
-trect = broom.get_rect()
-broomRect = broom.get_rect().move( tpos[0] - trect.width/2, tpos[1] - trect.height/2 )
-broomActiveRect = pygame.Rect((4, 41),(106, 82))
 
-# blit the broom to the screen and update the display
-screen.blit(broom, broomRect)
-
-####################### Set up the spiders #####################
-
-MaxSpiders = 5
-MinSpawnTime = 30
-
-
-# store a list of rectangles of the active spiders
-activeSpiders = []
-#ticksSinceLastSpawn = 0
-spider = Enemy(random.randint( 10, 560 ),random.randint( 40, 400 ))
-activeSpiders.append( spider )
-#screen.blit(spider)
-####################### Main Event Loop #########################
-# set up the refresh rectangle container
 refresh = []
 drawBkg(screen, text, refresh)
-
-# update the display before we start the main loop
-pygame.display.update()
-
+enemy_spider = Enemy(random.randint( 10, 770 ),random.randint( 40, 580 ))   
 # respond to mouse motion events until someone clicks a mouse or hits a key
 print("Entering main loop")
 while 1:
-
-    # handle events and erase things
+     # handle events and erase things
     for event in pygame.event.get():
-        if event.type == pygame.MOUSEMOTION:
-            # erase the existing broom
-            drawBkg(screen, text, refresh, broomRect)
+        # if event.type == pygame.MOUSEMOTION:
+        #     # erase the existing broom
+        #     drawBkg(screen, text, refresh, broomRect)
             
         if event.type == pygame.MOUSEBUTTONDOWN:
             sys.exit()
@@ -153,59 +110,12 @@ while 1:
 
         if event.type == pygame.QUIT:
             sys.exit()
-
-        # tx = random.randint( 10, 560 )
-        # ty = random.randint( 40, 400 )
-        # trect = spider.get_rect().move( tx, ty )
-        # activeSpiders.append( trect )
-
-    # draw the background behind each of the spiders
-    # this is erasing stuff
-    for item in activeSpiders:
-        drawBkg(screen, text, refresh, item)
-
-    # figure out if the broom is intersecting any of the spiders
-    tmpactive = []
-    for item in activeSpiders:
-        spiderCollisionBox = spiderActiveRect.move( item.left, item.top )
-        broomCollisionBox = broomActiveRect.move( broomRect.left, broomRect.top )
-        if broomCollisionBox.colliderect( spiderCollisionBox ):
-            continue
-        else:
-            tmpactive.append( item )
-    activeSpiders = tmpactive
-
-    # draw all of the spiders, the rectangle is already on the refresh
-    # list from the background draw
-    for item in activeSpiders:
-        screen.blit( spider, item )
         
-
-    # If the game is in focus, draw things
-    if pygame.mouse.get_focused():
-        pygame.mouse.set_visible(False)
-        tpos = pygame.mouse.get_pos()
-
-        # update the position of the cursor
-        broomRect = pygame.Rect( (tpos[0] - broomRect.width/2, tpos[1] - broomRect.height/2),
-                                  (broomRect.width, broomRect.height) )
-
-        # draw the broom in the location of the mouse
-        screen.blit(broom, broomRect)
-
-        # add to the refresh list
-        refresh.append(broomRect)
-    else:
-        pygame.mouse.set_visible(True)
-
-
     # update the parts of the screen that need it
-    pygame.display.update(refresh)
+    pygame.display.update( refresh )
 
     # clear out the refresh rects
     refresh = []
 
     # throttle the game speed to 30fps
     gameClock.tick(30)
-# done
-print("Terminating")
--- template.py
+++ template.py
@@ -22,7 +22,7 @@ pygame.init()
 try:
     pygame.font.init()
 except:
-    print ("Fonts unavailable")
+    print "Fonts unavailable"
     sys.exit()
 
 # create a game clock
@@ -46,8 +46,7 @@ afont = pygame.font.SysFont( "Helvetica", 20, bold=True )
 # render a surface with some text
 text = afont.render( "Clean up time", True, (0, 0, 0) )
 
-#create a start
-state = "start"
+
 ####################### Filling the Screen #########################
 
 # A function that draws all of the static background elements
@@ -61,7 +60,7 @@ def drawBkg(screen, text, refresh, rect=None):
 
         refresh.append( screen.get_rect() )
     else:
-        #screen.fill((255, 255, 255), rect)
+        screen.fill( (255, 255, 255), rect )
 
         # blit the text surface onto the screen if it is inside the rectangle
         screen.fill( (255, 255, 255), text.get_rect().move(10, 10).clip( rect ) )
@@ -81,19 +80,6 @@ def drawBkg(screen, text, refresh, rect=None):
         refresh.append( rect )
 
 
-class Enemy:  #Class for Spider character
-    def __init__(self,x, y):
-        self.image = spider
-        self.xPos = x
-        self.yPos = y
-        #self.trect = self.image.get_rect().move( x, y )
-
-    def random_move(self):
-        to_do = 1
-    def run_away(self,player_x, player_y):
-        to_do = 1
-
-
 ############## Setting up the Broom as a sprite ################
 
 # get the current mouse information, and make the cursor invisible if
@@ -110,7 +96,7 @@ broomRect = broom.get_rect().move( tpos[0] - trect.width/2, tpos[1] - trect.heig
 broomActiveRect = pygame.Rect((4, 41),(106, 82))
 
 # blit the broom to the screen and update the display
-screen.blit(broom, broomRect)
+screen.blit( broom, broomRect )
 
 ####################### Set up the spiders #####################
 
@@ -120,27 +106,25 @@ MinSpawnTime = 30
 
 # store a list of rectangles of the active spiders
 activeSpiders = []
-#ticksSinceLastSpawn = 0
-spider = Enemy(random.randint( 10, 560 ),random.randint( 40, 400 ))
-activeSpiders.append( spider )
-#screen.blit(spider)
+ticksSinceLastSpawn = 0
+
 ####################### Main Event Loop #########################
 # set up the refresh rectangle container
 refresh = []
-drawBkg(screen, text, refresh)
+drawBkg( screen, text, refresh )
 
 # update the display before we start the main loop
 pygame.display.update()
 
 # respond to mouse motion events until someone clicks a mouse or hits a key
-print("Entering main loop")
+print "Entering main loop"
 while 1:
 
     # handle events and erase things
     for event in pygame.event.get():
         if event.type == pygame.MOUSEMOTION:
             # erase the existing broom
-            drawBkg(screen, text, refresh, broomRect)
+            drawBkg( screen, text, refresh, broomRect )
             
         if event.type == pygame.MOUSEBUTTONDOWN:
             sys.exit()
@@ -151,15 +135,26 @@ while 1:
         if event.type == pygame.QUIT:
             sys.exit()
 
-        # tx = random.randint( 10, 560 )
-        # ty = random.randint( 40, 400 )
-        # trect = spider.get_rect().move( tx, ty )
-        # activeSpiders.append( trect )
+    # see if we need to add a spider
+    if len( activeSpiders ) < MaxSpiders and ticksSinceLastSpawn > MinSpawnTime:
+
+        # 10% chance per tick of a spawn
+        if random.random() < 0.1:
+            # pick a random location and add a new spider to the list
+            tx = random.randint( 10, 560 )
+            ty = random.randint( 40, 400 )
+            trect = spider.get_rect().move( tx, ty )
+            activeSpiders.append( trect )
+
+            # reset ticks since last spawn
+            ticksSinceLastSpawn = 0
+
+    ticksSinceLastSpawn += 1
 
     # draw the background behind each of the spiders
     # this is erasing stuff
     for item in activeSpiders:
-        drawBkg(screen, text, refresh, item)
+        drawBkg( screen, text, refresh, item )
 
     # figure out if the broom is intersecting any of the spiders
     tmpactive = []
@@ -188,21 +183,22 @@ while 1:
                                   (broomRect.width, broomRect.height) )
 
         # draw the broom in the location of the mouse
-        screen.blit(broom, broomRect)
+        screen.blit( broom, broomRect )
 
         # add to the refresh list
-        refresh.append(broomRect)
+        refresh.append( broomRect )
     else:
         pygame.mouse.set_visible(True)
 
 
     # update the parts of the screen that need it
-    pygame.display.update(refresh)
+    pygame.display.update( refresh )
 
     # clear out the refresh rects
     refresh = []
 
     # throttle the game speed to 30fps
     gameClock.tick(30)
+        
 # done
-print("Terminating")
+print "Terminating"
