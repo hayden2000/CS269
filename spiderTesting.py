de0f3c756d4ce180b4c456ca82f88d1724608e7d
sawyerstrong <sawyerstrong28@gmail.com>
2020-01-20 14:44:03 -0500

    Spinning Stroke of Death

--- spiderTesting.py
+++ spiderTesting.py
@@ -1,12 +1,12 @@
-import pygame
 import random
 import math
+import pygame
 import numpy
 # create a game clock
 gameClock = pygame.time.Clock()
 
 class Player:
-    def __init__(self, win, x, y, width=30, height=50, mass=1):
+    def __init__(self, win, x, y, width=30, height=50):
         self.x = x
         self.y = y
         self.win = win
@@ -72,31 +72,20 @@ class Player:
     
 
 class Enemy:
-    def __init__(self, win, x, y, width=90, height=50, mass=1):
+    def __init__(self, win, x, y, image, width=90, height=50):
         self.x = x
         self.y = y
         self.win = win
         self.width = width
         self.height = height
         self.maxVel = 2
-        self.isJump = False
-        self.jumpCount = 9
-        self.left = False
-        self.right = False
-        self.walkCount = 0
-        self.currentDirection = 0
-        self.moveDirection = 0
         self.downForce = 5
         self.currentSpeed = 1
         self.angle = 30
+        self.image = image
+        self.rotSurf = pygame.Surface( (800,600) )
 #list of image for walking right
 #list of image for walking left
-        self.gravity = 10
-
-    def getIsJump(self):
-        return self.isJump
-    def setIsJump(self, jump):
-        self.isJump = jump
     def getX(self):
         return self.x
     def getY(self):
@@ -131,10 +120,14 @@ class Enemy:
                 self.angle -= 25
             else:
                 self.angle +=0
+            self.rotate()
         if(self.x + self.width >= self.win.get_width() or self.x < 0):
             self.angle = -(90 -self.angle)
+            self.rotate()
         if(self.y + self.height >= self.win.get_height() or self.y < 0):
             self.angle = -(90 -self.angle)
+            self.rotate()
+        self.rotate()
         #print(self.angle)    
 
     def run_away(self, player_x, player_y):
@@ -147,23 +140,29 @@ class Enemy:
         #if collide move other way
         self.move_x(self.maxVel*ratio_x)
         self.move_y(self.maxVel*ratio_y)
+        #self.rotate()
+        
+    def rotate(self):
+        self.rotSurf.blit(self.image, self.image.get_rect())
+        self.rotSurf = pygame.transform.rotate(self.rotSurf, self.angle)
        
             
 def main():
     pygame.init()
     screenWidth = 800
     screenHeight = 600
-    win = pygame.display.set_mode((800,600)) 
+    win = pygame.display.set_mode((screenWidth,screenHeight)) 
+    spider_img = pygame.image.load( "Spider.png" ).convert_alpha()
     pygame.display.set_caption("Test")
     player = Player(win, 300, 200)
-    spider = Enemy(win,300, 500)
+    spider = Enemy(win, 300, 500, spider_img)
     frame = 0
     run = True
     while run:
         frame+=1
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
+            if event.type == pygame.QUIT:
+                run = False
         radius = math.sqrt((float(player.getX())-float(spider.getX()))**2 +(float(player.getY())- float(spider.getY()))**2)
         #print (radius)
         if radius >= 150:
