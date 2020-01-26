ae58b1ddeeb93ad1df3306e80831e9b1f47f1365
Bmart22 <56095521+Bmart22@users.noreply.github.com>
2020-01-26 15:58:40 -0500

    Door animation

--- Door.py
+++ Door.py
@@ -2,20 +2,37 @@
 
 import pygame
 
-door_img = pygame.image.load("Assets/door.png")
+door_img = pygame.image.load("Assets/Door_Sprite.png")
 
 class Door:
     def __init__(self, midbottom, exit):
-        self.image = door_img
+        self.currentSprite = 0
+        self.lastTicks = 0
+#        self.image = door_img
+        self.load_images()
+        self.image = self.opennning[0]
         
         self.center = ( midbottom[0], midbottom[1] - (self.image.get_height()/2) )
-        self.rect = door_img.get_rect()
+        self.rect = self.image.get_rect()
         self.rect.center = self.center
         
         self.exit = exit
         self.entrance = not exit
         self.unlocked = False
-    
+       
+    #triggers next level load
+    def get_images(self, a, b, wid, hei):
+        sprite = pygame.image.load('Assets/Door_Sprite.png').convert()
+        getimage = pygame.Surface((wid,hei))
+        getimage.blit(sprite,(0,0),(a,b,wid,hei))
+        getimage = pygame.transform.scale(getimage,(wid//6, hei//6))
+        return getimage
+
+    def load_images(self):
+        self.opennning = [self.get_images(20,20,413,333),self.get_images(20,373,408,326),self.get_images(453,369,410,327),self.get_images(453,20,410,329)]
+        for pic in self.opennning:
+            pic.set_colorkey((0,0,0))
+
     #triggers next level load
     def win(self, player):
         if(self.exit and player.rect.collidepoint( self.center ) and self.unlocked):
@@ -30,3 +47,15 @@ class Door:
             pygame.mixer.music.set_volume(1)
             pygame.mixer.music.play()
         
+        self.doorMotion()
+        
+    def doorMotion(self):
+        nowTicks = pygame.time.get_ticks()
+        if self.currentSprite < 3:
+            if nowTicks - self.lastTicks > 300:
+                self.lastTicks = nowTicks
+                self.currentSprite = self.currentSprite + 1
+                #bottom = self.rect.bottom
+                self.image = self.opennning[self.currentSprite]
+                #self.rect = self.image.get_rect()
+                #self.rect.bottom = bottom
