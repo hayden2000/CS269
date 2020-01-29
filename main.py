752af5e5b59d540069064261fc3f751f49e014d0
Bmart22 <56095521+Bmart22@users.noreply.github.com>
2020-01-29 13:56:41 -0500

    Play the lose animation

--- main.py
+++ main.py
@@ -857,10 +857,11 @@ def endScreen(win, score, level):
     if win == False:
         for y in range(3):
             for x in range(9):
-                getimage = pygame.Surface((wid,hei))
+                getimage = pygame.Surface((800,600))
                 getimage.blit(loseAnimate,(0,0),((20 * (x+1)) + (800 * x), (20 * (y+1))+(600*y), 800, 600))
                 screen.blit(getimage, (0,0))
-                gameClock.tick(30)
+                gameClock.tick(10)
+                pygame.display.update()
 
       
     while running:
